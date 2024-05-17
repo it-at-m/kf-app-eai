f03e54a37d348a73e317bc728200d378eb607202
matthias.karl <matthias.karl@muenchen.de>
2024-05-17 15:30:10 +0200

    added required scopes to swagger-ui

--- src/main/java/de/muenchen/rbs/kitafindereai/config/SecurityConfiguration.java
+++ src/main/java/de/muenchen/rbs/kitafindereai/config/SecurityConfiguration.java
@@ -31,6 +31,7 @@ import de.muenchen.rbs.kitafindereai.api.KitaAppApiController;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.OAuthFlow;
 import io.swagger.v3.oas.annotations.security.OAuthFlows;
+import io.swagger.v3.oas.annotations.security.OAuthScope;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import lombok.extern.slf4j.Slf4j;
 
@@ -46,6 +47,10 @@ import lombok.extern.slf4j.Slf4j;
 public class SecurityConfiguration {
 
     private static final String AUD_CLAIM = "aud";
+
+    public static final String SCOPE_LHM_EXTENDED = "LHM_Extended";
+    public static final String SCOPE_ROLES = "roles";
+    public static final String SCOPE_OPENID = "openid";
 
     /** Security for {@link InternalApiController} */
     @Bean
@@ -144,8 +149,12 @@ public class SecurityConfiguration {
     /** Swagger-API config for security */
     @Configuration
     @Profile("!no-security")
-    @SecurityScheme(name = "ApiClient", type = SecuritySchemeType.OAUTH2, flows = @OAuthFlows(clientCredentials = @OAuthFlow(tokenUrl = "${app.security.token-url}")))
-    @SecurityScheme(name = "InternalLogin", type = SecuritySchemeType.OAUTH2, flows = @OAuthFlows(authorizationCode = @OAuthFlow(tokenUrl = "${app.security.token-url}", authorizationUrl = "${app.security.authorization-url}", refreshUrl = "${app.security.token-url}")))
+    @SecurityScheme(name = "ApiClient", type = SecuritySchemeType.OAUTH2, flows = @OAuthFlows(clientCredentials = @OAuthFlow(tokenUrl = "${app.security.token-url}", scopes = {
+            @OAuthScope(name = SCOPE_LHM_EXTENDED), @OAuthScope(name = SCOPE_ROLES) })))
+    @SecurityScheme(name = "InternalLogin", type = SecuritySchemeType.OAUTH2, flows = @OAuthFlows(authorizationCode = @OAuthFlow(tokenUrl = "${app.security.token-url}", authorizationUrl = "${app.security.authorization-url}", refreshUrl = "${app.security.token-url}", scopes = {
+            @OAuthScope(name = SCOPE_LHM_EXTENDED), @OAuthScope(name = SCOPE_ROLES),
+            @OAuthScope(name = SCOPE_OPENID) })))
+
     public class SpringdocConfig {
     }
 
